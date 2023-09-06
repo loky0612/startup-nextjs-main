32d8fbd6a88a0ca55dd385ce976b9006753e859e
LOKESH M <lokeshmalairaj@gmail.com>
2023-09-06 11:42:40 +0530

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,27 @@
-"use client";
+"use client"
+import React, { useEffect, useState } from "react";
 import { Providers } from "./providers";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
 import "node_modules/react-modal-video/css/modal-video.css";
 import "../styles/index.css";
+import Loader from '../components/Loader/Loader';
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a data fetch or any other async operation
+    setTimeout(() => {
+      setIsLoading(false); // Set isLoading to false after 2 seconds
+    }, 2000);
+  }, []);
+
   return (
     <html suppressHydrationWarning lang="en">
       {/*
@@ -21,13 +32,19 @@ export default function RootLayout({
 
       <body className="dark:bg-black">
         <Providers>
-          <Header />
-          {children}
-          <Footer />
-          <ScrollToTop />
+          {isLoading ? ( // If isLoading is true, display the loader
+            <Loader />
+          ) : (
+            // If isLoading is false, display the actual page content
+            <>
+              <Header />
+              {children}
+              <Footer />
+              <ScrollToTop />
+            </>
+          )}
         </Providers>
       </body>
     </html>
   );
 }
-
--- next.config.js
+++ next.config.js
@@ -3,6 +3,20 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(mp4|webm)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          outputPath: 'static/videos/', // You can configure the output path
+        },
+      },
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
+
